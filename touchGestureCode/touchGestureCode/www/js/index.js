992ba536030f25c9ee69de273890044c92e09841
Jordan Meacham <jordym68@gmail.com>
2014-11-07 11:30:53 +0000

    colour change

--- touchGestureCode/touchGestureCode/www/js/index.js
+++ touchGestureCode/touchGestureCode/www/js/index.js
@@ -7,6 +7,7 @@ $(document).on("pagecreate","#pageone",function(){
  	});                       
 
 	$('#taptext').on("tap",function(){
+		$(this).css('color', 'red');
     	alert("Stop tapping!");
  	}); 
 
